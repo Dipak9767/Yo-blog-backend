e19984cf97ca75bd2a9b1683e12d4540c97b8a03
Dipak Firake <dipakfirake9767@gmail.com>
2023-04-23 17:16:30 +0530

    mongo url added

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://dipakfirake9767:dipak123@cluster0.uwhrw.mongodb.net/Blog')
+mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log('DB is Connected'))
 .catch((e)=> console.log(e))
